15f6a4cc30d58b51e01f91877bd452cd12f933e3
George Harland <georgeharland12@gmail.com>
2022-03-31 22:28:45 +0100

    Created Spring Boot project

---
+++ src/main/java/com/votingforum/voting_forum/VotingForumApplication.java
@@ -0,0 +1,13 @@
+package com.votingforum.voting_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/votingforum/voting_forum/VotingForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votingforum.voting_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
